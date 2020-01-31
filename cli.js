74601b11929184e3f863c0e74031fa7fbd471190
Jaga Santagostino <jagasantagostino@gmail.com>
2020-01-31 13:35:11 +0100

    initial

---
+++ cli.js
@@ -0,0 +1,51 @@
+#!/usr/bin/env node
+
+const { Select } = require("enquirer");
+const yaml = require("yaml");
+const fs = require("fs");
+const path = require("path");
+const { execSync } = require("child_process");
+
+function getFunctionNames() {
+	return Object.keys(parseServerlessFile().functions);
+}
+
+function parseServerlessFile() {
+	const p = path.join(process.cwd(), "serverless.yml");
+	try {
+		const file = fs.readFileSync(p, "utf8");
+		return yaml.parse(file);
+	} catch (e) {
+		if (e.code === "ENOENT") {
+			console.error(`cannot find file: ${p}`);
+			process.exit(1);
+		}
+
+		console.error(e);
+	}
+}
+
+async function run() {
+	const prompt = new Select({
+		name: "fnName",
+		message: "Select function to run",
+		choices: getFunctionNames()
+	});
+
+	try {
+		const fnName = await prompt.run();
+		const cmd = `sls invoke local -f ${fnName}`;
+
+		console.log(`\nrunning: ${cmd}`);
+
+		const result = execSync(cmd, {
+			encoding: "utf8"
+		});
+
+		console.log(`\nresult: \n \n${result}`);
+	} catch (e) {
+		console.error(e);
+	}
+}
+
+run();
---
+++ readme.md
@@ -0,0 +1,5 @@
+# slocal
+
+select which [Serverless](https://serverless.com/) function to invoke locally
+
+[![asciicast](https://asciinema.org/a/SMutK7lUDkfafVZTB1XrQArLx.svg)](https://asciinema.org/a/SMutK7lUDkfafVZTB1XrQArLx)
